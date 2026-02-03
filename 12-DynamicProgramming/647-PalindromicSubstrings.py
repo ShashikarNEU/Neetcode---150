1a0caf153a3e1e99d990b0da5647ddbd7d663618
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2026-02-03 00:21:19 -0500

    DP Appoarch for leetcode 647

--- 12-DynamicProgramming/647-PalindromicSubstrings.py
+++ 12-DynamicProgramming/647-PalindromicSubstrings.py
@@ -24,3 +24,32 @@ if __name__ == "__main__":
     s = Solution()
     print(s.countSubstrings("aaa"))
     print(s.countSubstrings("abbd"))
+
+
+# DP Approach
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        count = 0
+        
+        # All substrings of length 1 are palindromes
+        for i in range(n):
+            dp[i][i] = True
+            count += 1
+            
+        # Check for substrings of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                count += 1
+                
+        # Check for substrings of length 3 or more
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    count += 1
+                    
+        return count
